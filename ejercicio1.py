b41d1ed6ca6fecbc69b24851e07564d03871ceb5
Nicolas Posa <nicoposa57@gmail.com>
2020-12-12 21:32:04 -0300

    iteraciones

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,6 +6,6 @@ edad_usr_2 = int(input(f'Y la edad de {name_usr_2}?: '))
 if edad_usr_1 > edad_usr_2:
     print(f'Sabias que {name_usr_1} es mayor que {name_usr_2}?')
 elif edad_usr_1 < edad_usr_2:
-    print(f'Sabisa que {name_usr_2} es mayor que {name_usr_1}')
+    print(f'Sabias que {name_usr_2} es mayor que {name_usr_1}')
 else:
     print(f'Sabias que {name_usr_1} y {name_usr_2} tienen la misma edad')
