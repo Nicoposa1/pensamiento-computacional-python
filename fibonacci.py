890bda253274dcbe5fa860f2817283adac1f907c
Nicolas Posa <nicoposa57@gmail.com>
2021-01-02 17:36:12 -0300

    tuple

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,4 @@
-def fibonacci(n):
-    if n == 0 or n == 1:
-        return 1
-
-    return fibonacci(n - 1) + fibonacci(n - 2)
+def sumar(x, y): return x + y
 
 
-n = int(input("Escriba el mes que desea conocer: "))
-print(f'En el mes número {n} habrán {fibonacci(n)} conejos')
+sumar(2, 3)
